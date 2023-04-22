e2464d9eb4bf1d77c046ba58d154e9a48898b660
ArtemSap <artik21rus21@gmail.com>
2023-04-22 11:49:39 +0300

    Выпилил ненужный кусок кода, который сейчас не используется

--- Assets/TerrainDefform.cs
+++ Assets/TerrainDefform.cs
@@ -70,7 +70,6 @@ public class TerrainDefform : MonoBehaviour
         ObjectInformation objinfo = obj.GetComponent<ObjectInformation>();
 
         DeformByIntersection(objinfo.ObjectVelocity);
-        //DeformTerrainByObjects(collision);
     }
 
     private void OnCollisionStay(Collision collision)
@@ -79,7 +78,6 @@ public class TerrainDefform : MonoBehaviour
         ObjectInformation objinfo = obj.GetComponent<ObjectInformation>();
 
         DeformByIntersection(objinfo.ObjectVelocity);
-        //DeformTerrainByObjects(collision);
     }
 
     public void DeformByIntersection(Vector3 velocity)
@@ -548,82 +546,9 @@ public class TerrainDefform : MonoBehaviour
         return final_pressure;
     }
 
-    void DeformTerrainByObjects(Collision collision)
-    {
-        List<Vector2> list_height_pix = GetHeightPix_collide(collision);
-
-        //Находит минимум и максимумы по обоим осям, чтобы считать только маленький "кусочек" координат террейна
-        //Сделано для того, чтобы не считывать огромную матрицу высот для оптимизации
-        int min_x = terrain.terrainData.heightmapResolution;
-        int min_y = terrain.terrainData.heightmapResolution;
-        int max_x = 0, max_y = 0;
-        foreach (var pix in list_height_pix)
-        {
-            if (pix.x < min_x) min_x = (int)pix.x;
-            if (pix.y < min_y) min_y = (int)pix.y;
-            if (pix.x > max_x) max_x = (int)pix.x;
-            if (pix.y > max_y) max_y = (int)pix.y;
-        }
-        max_x++;
-        max_y++;
-
-        var heights_ter = terrain.terrainData.GetHeights(min_y, min_x, max_y - min_y, max_x - min_x);
-
-        //Потенциально этот цикл можно будет распараллелить используя шейдер
-        //Цикл с деформацией террейна по заданой логике
-        foreach (var pix in list_height_pix)
-        {
-            int index_x = (int)(pix.x - min_x);
-            int index_y = (int)(pix.y - min_y);
-
-            var x = heights_ter[index_x, index_y] - 0.0001f; // <--- Непосредственно здесь происходит деформация
-
-            var min = default_height[(int)pix.x, (int)pix.y] - 0.001f;
-            var max = default_height[(int)pix.x, (int)pix.y];
-
-            heights_ter[index_x, index_y] = Mathf.Clamp(x, min, max);
-        }
-
-        terrain.terrainData.SetHeights(min_y, min_x, heights_ter);
-    }
-
-    List<Vector2> GetHeightPix_collide(Collision collision)
-    {
-        List<Vector2> list_height_pix = new List<Vector2>();
-
-        List<ContactPoint> contacts = new List<ContactPoint>();
-        var cont = collision.GetContacts(contacts);
-
-        foreach (ContactPoint contact in contacts)
-        {
-            if (contact.thisCollider != null)
-            {
-                //координаты на террейне
-                var local_x = (contact.point.x - terrain_pos_global.x) + terrain.terrainData.size.x / 2;
-                var local_z = (contact.point.z - terrain_pos_global.z) + terrain.terrainData.size.z / 2;
-
-                //процентное соотношение где соприкасновение произошло относительно лок. координат террейна
-                var per_x = local_x / terrain.terrainData.size.x;
-                var per_z = local_z / terrain.terrainData.size.z;
-
-                //Переводим координаты на террейне в пиксели у карты высот
-                Vector2 height_vert = new Vector2((int)(terrain.terrainData.heightmapResolution * per_z),
-                                                  (int)(terrain.terrainData.heightmapResolution * per_x));
-
-                if (!list_height_pix.Contains(height_vert))
-                {
-                    //Debug.DrawRay(contact.point, new Vector3(0, 1, 0), Color.green, 100);
-                    list_height_pix.Add(height_vert);
-                }
-            }
-        }
-        return list_height_pix;
-    }
-
     public void ResetTerrain()
     {
         terrain.terrainData.SetHeights(0, 0, default_height);
         coef_correctness = 0;
     }
-    
 }
